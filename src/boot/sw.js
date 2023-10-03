718393e0789c433afb3b559c21b00a938e0c031c
Ángel Contreras <angelcnsk@gmail.com>
2023-10-02 22:21:53 -0600

    remove custom sw

--- src/boot/sw.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/src-pwa/sw.js')
-      .then(() => {
-        console.log('Service worker registered')
-      })
-      .catch(error => {
-        console.error('Error registering service worker:', error)
-    })
-}
